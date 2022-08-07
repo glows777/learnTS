df85c7993e38f606d12e083379547ed6c678e46d
glows777 <1914426389@qq.com>
2022-08-07 16:06:46 +0800

    chore: 纠正第4题法2的解释以及添加一些高级类型判断

--- 5/src/lib.ts
+++ 5/src/lib.ts
@@ -18,10 +18,82 @@ type bbb = Extract<"a" | "b" | "c", "a" | "d">;
 
 type classmate = Omit<myType, "stuNo">;
 
-type myGetParam<func extends Function> = func extends (...args: infer Args) => unknown ? Args : never;
+type myGetParam<func extends Function> = func extends (
+  ...args: infer Args
+) => unknown
+  ? Args
+  : never;
 
 function add(a: number, b: number): number {
   return a + b;
 }
 
 type addParamType = myGetParam<typeof add>;
+// 合并两个数组
+type zipp<One extends unknown[], Other extends unknown> = One extends [
+  infer OneFirst,
+  ...infer OneRest
+]
+  ? Other extends [infer OtherFirst, ...infer OtherRest]
+    ? [OneFirst, OtherFirst, ...zipp<OneRest, OtherRest>]
+    : never
+  : never;
+
+type ttt = Promise<Promise<Promise<Record<string, any>>>>;
+type deep<T> = T extends Promise<infer R> ? deep<R> : T;
+
+type a = deep<ttt>;
+
+type IsEqual<A, B> = (A extends B ? true : false) &
+  (B extends A ? true : false);
+// 是否包含某个元素
+type Includes<Arr extends unknown[], FindItem> = Arr extends [
+  infer firstItem,
+  ...infer Rest
+]
+  ? IsEqual<firstItem, FindItem> extends true
+    ? true
+    : Includes<Rest, FindItem>
+  : false;
+
+// 移除数组中的某个元素
+type removeItem<
+  Arr extends unknown[],
+  item,
+  Result extends unknown[] = []
+> = Arr extends [infer firstItem, ...infer Rest]
+  ? IsEqual<firstItem, item> extends true
+    ? removeItem<Rest, item, Result>
+    : removeItem<Rest, item, [...Result, firstItem]>
+  : Result;
+
+type test = removeItem<[1, 2, 3, 4], 2>;
+
+// 深度遍历，修改属性
+// ts 只有类型被用到的时候才会做类型计算，所以前面加一段T extends any ? {...} : never来触发计算
+type deepReadOnly<T extends Record<string, any>> = T extends any
+  ? {
+      readonly [K in keyof T]: T[K] extends object
+        ? T[K] extends Function
+          ? T[K]
+          : deepReadOnly<T[K]>
+        : T[K];
+    }
+  : never;
+
+type obj = {
+  a: {
+    b: {
+      c: {
+        f: () => "dong";
+        d: {
+          e: {
+            guang: string;
+          };
+        };
+      };
+    };
+  };
+};
+
+type test2 = deepReadOnly<obj>;
--- 6/src/4.ts
+++ 6/src/4.ts
@@ -14,14 +14,14 @@ type StringKeysOnly = ConditionalPick<Example, string>;
 //=> {a: string}
 
  * * 法一： 采用先找出符合条件的属性的key，然后遍历这个包含符合条件的所有key形成的联合类型，返回一个新类型 
- * * 法二： 同理，只不过用的是as断言，更加直接 
+ * * 法二： 同理，只不过用的是as，更加直接 
 */
 type helper<T, K extends keyof T, U> = K extends any ? (T[K] extends U ? K : never) : never; // 获取符合这个类型的所有key，返回一个联合类型
 
 type ConditionalPick<T, U> = {
     [K in helper<T, keyof T, U>]: T[K];
 }
-/** 这个也可以 采用as断言
+/** 这个也可以 采用as去转化key
 type ConditionalPick<V, T> = {
   [K in keyof V as V[K] extends T ? K : never]: V[K];
 };
