e3f9cd9c167862e9222ea869a828557028f3635e
glows777 <1914426389@qq.com>
2023-01-29 17:58:34 +0800

    feat: 复习1-4题

---
+++ all.ts
@@ -0,0 +1,64 @@
+// ! 链接在这里
+// https://github.com/semlinker/awesome-typescript/issues?page=2&q=is%3Aissue+is%3Aopen
+type Simplify<T> = {
+  [K in keyof T]: T[K]
+}
+
+
+// ? 2 函数重载
+function func(a: number, b: number): number;
+function func(a: string, b: string): string;
+function func(a: string | number, b: string | number): string | number {
+  if (typeof a === "string") {
+    return a + ":" + b;
+  } else {
+    return (a as number) + (b as number);
+  }
+}
+
+// ? 3
+type SetOptional<T, K extends keyof T> = Partial<Pick<T, K>> & Pick<T, Exclude<keyof T, K>>
+type Foo = {
+	a: number;
+	b?: string;
+	c: boolean;
+}
+// 测试用例：
+type SomeOptional = Simplify<SetOptional<Foo, 'a' | 'b'>>
+
+
+// ? 4
+type ConditionalPick<T, U> = {
+  [K in keyof T as T[K] extends U ? K : never]: T[K]
+}
+interface Example {
+  a: string;
+  b: string | number;
+  c: () => void;
+  d: {};
+}
+// 测试用例：
+type StringKeysOnly = ConditionalPick<Example, string>
+
+
+// ? 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
